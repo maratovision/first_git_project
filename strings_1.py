041f8bf787969c834dc6abf2b1a164068c49efc6
Maratovision <maratovision@gmail.com>
2020-11-30 20:09:31 +0600

    new_file

---
+++ strings_1.py
@@ -0,0 +1,15 @@
+def register(username, password, check_password):
+    # "Пароль начинается с заглавной буквы, состоит из цифр и букв, длина больше 10 символом"
+    # Validate password
+    if password.istitle() and not password.isdigit() and not password.isalpha() and len(password) > 10:
+        if password == check_password:
+            print("Регистрация")
+        else:
+            print("Пароли не совпадают")
+    else:
+        print("Соблюдайте правила")
+username = input("Введите имя: ")
+password = (input("Введите пароль: "))
+check_password = (input("Подтвердите пароль: "))
+
+register(username, password, check_password)
