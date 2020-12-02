13af94e98e89a7f5bc16a733a3c49e90c53247a8
Maratovision <maratovision@gmail.com>
2020-12-02 20:30:27 +0600

    Task_about_Sofiya

--- Sofiya_task.py
+++ Sofiya_task.py
@@ -1,1 +1,11 @@
 with open("spam.txt", "r") as file1:
+    file2 = open("dont_read.txt", "w")
+    list1 = file1.readlines()
+    for line in list1:
+        line = line.strip()
+        if "HELP" in line or "urgent" in line or "HELP" in line:
+            file2.write(line + "\n")
+        elif line.isupper() and line.endswith("!!!") or line.isupper() or line.endswith("!!!"):
+            file2.write(line + "\n")
+
+    print("Find")
