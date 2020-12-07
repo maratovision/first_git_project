02335cd9c0a58a055b352fea546ddb3fbe44a92e
Maratovision <maratovision@gmail.com>
2020-12-07 20:02:55 +0600

    tast_about_register

--- main_task.py
+++ main_task.py
@@ -1,14 +1,11 @@
-def register(username, password, check_password):
-    if password == check_password:
-        if 8 < len(username) < 40 and 8 < len(password) < 16:
-            print("Успешно")
-            with open("database.txt", "w") as db1:
-                db1.write(username + '\n' + password)
-                code = 1234
-                return code
-        else:
-            print("Неправильная длина")
-    else:
-        print("Пароли не совпадают")
-
-register("testname123", "password123", "password123")
+with open("database.txt", "w") as bd1:
+    print(bd1)
+#
+# def auth(username, password):
+#     if username and password in bd1:
+#         if 8 < len(username) > 16 and 8 < len(password) > 16:
+#             print("Успех")
+#         elif:
+#             print("Длина не соответствует")
+#     elif:
+#         print("Неверное имя пользователя или пароль")
