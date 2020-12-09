758df7236f592674571fcd1836099e9440d87cc3
Maratovision <maratovision@gmail.com>
2020-12-09 20:19:46 +0600

    tast_about_register

--- main_task.py
+++ main_task.py
@@ -1,11 +1,10 @@
-with open("database.txt", "w") as bd1:
-    print(bd1)
-#
-# def auth(username, password):
-#     if username and password in bd1:
-#         if 8 < len(username) > 16 and 8 < len(password) > 16:
-#             print("Успех")
-#         elif:
-#             print("Длина не соответствует")
-#     elif:
-#         print("Неверное имя пользователя или пароль")
+name = "Maksat"
+if len(name) < 100:
+        list2 = []
+        for alpha in name:
+            if alpha not in list2:
+                list2.append(alpha)
+        if len(list2) % 2 == 0:
+            print("on")
+        else:
+            print("ona")
