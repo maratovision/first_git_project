2382fd2d974f7ee4b9e09d2e37178f4c18a45935
Maratovision <maratovision@gmail.com>
2020-12-11 20:37:26 +0600

    tast_about_hipster

--- hipster_task.py
+++ hipster_task.py
@@ -1,17 +1,7 @@
 socks = list(map(int,input().split()))
-a = socks[0]
-b = socks[1]
-count_fash = 0
-count_notfash = 0
-if a > b:
-    a = a - b
-    if a > 1:
-        count_notfash = a // 2
-    print(b, count_notfash)
-elif a < b:
-    b = b - a
-    if b > 1:
-        count_notfash = b // 2
-    print(a, count_notfash)
-    else:
-        b = 0
+red = socks[0]
+blue = socks[1]
+count_fash = min(red, blue)
+count_notfash = (red - min(red, blue) + blue - min(red, blue)) / 2
+
+print(count_fash, int(count_notfash))
