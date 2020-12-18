e4bbfc2b8d73a6c24c0d45ea6649bfd19bbf7cdb
Maratovision <maratovision@gmail.com>
2020-12-18 21:09:09 +0600

    21

--- task_about_caps.py
+++ task_about_caps.py
@@ -2,7 +2,7 @@ string = input()
 if string.isupper() or string.islower():
     string = string.swapcase()
 elif string[0].islower() or string[0].isupper():
-    string = string
+    string = string.capitalize()
 else:
     string = string.capitalize()
 print(string)
